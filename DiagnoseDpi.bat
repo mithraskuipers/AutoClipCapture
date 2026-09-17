aaebd0621bcca7a6dc6130ee803e60fece4cc6a7
Mithras Kuipers <mithraskuipers@gmail.com>
2026-09-17 08:23:05 +0200

    update

---
+++ DiagnoseDpi.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -NoProfile -NoLogo -Command "Unblock-File -Path '%~dp0DiagnoseDpi.ps1'" >nul 2>&1
+powershell -STA -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0DiagnoseDpi.ps1"
