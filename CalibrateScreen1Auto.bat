d11d5e6de754b41740a9532c283d109e134f0e9e
Mithras Kuipers <mithraskuipers@gmail.com>
2026-09-18 08:43:49 +0200

    update

---
+++ CalibrateScreen1Auto.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -NoProfile -NoLogo -Command "Unblock-File -Path '%~dp0CalibrateScreen1Auto.ps1'" >nul 2>&1
+powershell -STA -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0CalibrateScreen1Auto.ps1"
